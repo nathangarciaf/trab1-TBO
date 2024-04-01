a615dfd8e42b88d01187b1d6340079b4ae1bf05e
nathangarciaf <nathangarfreitas@gmail.com>
2024-03-31 22:31:24 -0300

    read file

--- cluster.c
+++ cluster.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(){
+int main(int argc, char *argv[]){
+    if(argc < 4){
+        printf("ERRO\n");
+        return 0;
+    }
+
+    
+    FILE *file = fopen(argv[1], "r");
+    if(file == NULL){
+        printf("ARQUIVO DE ENTRADA INEXISTENTE!\n");
+        return 0;
+    }
+
+    FILE *saida = fopen(argv[3], "w");
+    char *s = (char*)calloc(100,sizeof(char));
+    while(!feof(file)){
+        fscanf(file, "%s\n",s);
+        fprintf(saida,"%s\n",s);
+    }
+    free(s);
     
 
+    fclose(file);
+    fclose(saida);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 all: trab1
 
 trab1:
-	gcc -o -g trab1 *.c
+	gcc -Wall -o trab1 *.c
