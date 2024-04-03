60f49898ecd0cbae514dc6e86b04020d92af42d3
nathangarciaf <nathangafreitas@gmail.com>
2024-04-03 15:52:22 -0300

    getline add

---
+++ cluster.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    if(argc < 4){
+        printf("ERRO\n");
+        return 0;
+    }
+
+    
+    FILE *file = fopen(argv[1], "r");
+    if(file == NULL){
+        printf("ARQUIVO DE ENTRADA INEXISTENTE!\n");
+        return 0;
+    }
+
+    FILE *saida = fopen(argv[3], "w");
+
+    char *entrada = NULL;
+    size_t size = 0;
+    int len = getline(&entrada, &size, file);
+    entrada[len-1] = '\0';
+
+    while(len > 0){
+        char *subs = strtok(entrada,",");
+        while(subs){
+            printf("%s\n",subs);
+            subs = strtok(NULL, ",");
+        }
+
+        size++;
+        len = getline(&entrada, &size, file);
+        entrada[len-1] = '\0';
+    }
+    
+    free(entrada);
+
+    fclose(file);
+    fclose(saida);
+    return 0;
+}
